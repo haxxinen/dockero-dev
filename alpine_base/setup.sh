69b37977a4c35ffe6d8d55f0052822d841ff8051
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2019-10-16 18:58:24 +0200

    Fixed SSH install conditional.

--- alpine_base/setup.sh
+++ alpine_base/setup.sh
@@ -3,6 +3,6 @@
 apk --update --no-cache add bind-tools musl-dev ncurses-dev wget curl sudo zip unzip ca-certificates supervisor
 echo "Europe/Zurich" > /etc/timezone
 
-[ ${INSTALL_SSH} -eq 1 ] && apk --update --no-cache add openssh && \
+[ ${INSTALL_SSH} -eq 1 ] && (apk --update --no-cache add openssh && \
 adduser -D -s /bin/sh -h /home/${SSH_USER} -g ${SSH_USER} ${SSH_USER} && \
-ssh-keygen -A -b 4096 && echo "${SSH_USER}:${SSH_PASS}" | chpasswd
+ssh-keygen -A -b 4096 && echo "${SSH_USER}:${SSH_PASS}" | chpasswd ) || (sleep 0)
