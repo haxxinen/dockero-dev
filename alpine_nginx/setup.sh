14d3a810d242ceaa8fd58f3fdab7da7e4643f44c
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2019-10-21 02:53:33 +0200

    Setup script to remove temp file.

--- alpine_nginx/setup.sh
+++ alpine_nginx/setup.sh
@@ -5,5 +5,6 @@ helpers=" make g++ wget curl"
 apk --update --no-cache add openssl-dev pcre-dev \
 zlib-dev supervisor shadow `echo "$helpers"` && \
 sh /tmp/nginx_installer.sh && apk del `echo "$helpers"`
+rm /tmp/nginx_installer.sh
 
 adduser -D -s /sbin/nologin -h /dev/null -g nginx nginx
