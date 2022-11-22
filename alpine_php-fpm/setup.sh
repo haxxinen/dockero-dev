f447e20a327485302e980bd851383862fc77f287
haxxinen <32751905+haxxinen@users.noreply.github.com>
2022-11-22 15:00:00 +0100

    Update setup.sh

--- alpine_php-fpm/setup.sh
+++ alpine_php-fpm/setup.sh
@@ -2,46 +2,46 @@
 
 apk --update --no-cache add \
 	curl \
-	php7 \
-	php7-bcmath \
-	php7-bz2 \
-	php7-ctype \
-	php7-curl \
-	php7-dom \
-	php7-fileinfo \
-	php7-fpm \
-	php7-gd \
-	php7-gettext \
-	php7-gmp \
-	php7-iconv \
-	php7-intl \
-	php7-json \
-	php7-mbstring \
-	php7-mcrypt \
-	php7-mysqli \
-	php7-mysqlnd \
-	php7-odbc \
-	php7-opcache \
-	php7-openssl \
-	php7-pdo \
-	php7-pdo_dblib \
-	php7-pdo_mysql \
-	php7-pdo_odbc \
-	php7-pdo_pgsql \
-	php7-pdo_sqlite \
-	php7-phar \
-	php7-posix \
-	php7-session \
-	php7-simplexml \
-	php7-soap \
-	php7-sqlite3 \
-	php7-tokenizer \
-	php7-xdebug \
-	php7-xml \
-	php7-xmlreader \
-	php7-xmlrpc \
-	php7-xmlwriter \
-	php7-zip
+	php8 \
+	php8-bcmath \
+	php8-bz2 \
+	php8-ctype \
+	php8-curl \
+	php8-dom \
+	php8-fileinfo \
+	php8-fpm \
+	php8-gd \
+	php8-gettext \
+	php8-gmp \
+	php8-iconv \
+	php8-intl \
+	php8-json \
+	php8-mbstring \
+	php8-mcrypt \
+	php8-mysqli \
+	php8-mysqlnd \
+	php8-odbc \
+	php8-opcache \
+	php8-openssl \
+	php8-pdo \
+	php8-pdo_dblib \
+	php8-pdo_mysql \
+	php8-pdo_odbc \
+	php8-pdo_pgsql \
+	php8-pdo_sqlite \
+	php8-phar \
+	php8-posix \
+	php8-session \
+	php8-simplexml \
+	php8-soap \
+	php8-sqlite3 \
+	php8-tokenizer \
+	php8-xdebug \
+	php8-xml \
+	php8-xmlreader \
+	php8-xmlrpc \
+	php8-xmlwriter \
+	php8-zip
 
 adduser -D -s /sbin/nologin -h /dev/null -g php-fpm php-fpm
 echo "chdir = $CHDIR" >> /etc/supervisord.conf
