c8be69f59d7066ac87197f8fe7c993c913b2d33f
haxxinen <32751905+haxxinen@users.noreply.github.com>
2023-06-15 18:40:54 +0200

    Update setup.sh

--- alpine_php-fpm/setup.sh
+++ alpine_php-fpm/setup.sh
@@ -2,45 +2,45 @@
 
 apk --update --no-cache add \
 	curl \
-	php8 \
-	php8-bcmath \
-	php8-bz2 \
-	php8-ctype \
-	php8-curl \
-	php8-dom \
-	php8-fileinfo \
-	php8-fpm \
-	php8-gd \
-	php8-gettext \
-	php8-gmp \
-	php8-iconv \
-	php8-intl \
-	php8-json \
-	php8-mbstring \
-	php8-pecl-mcrypt \
-	php8-mysqli \
-	php8-mysqlnd \
-	php8-odbc \
-	php8-opcache \
-	php8-openssl \
-	php8-pdo \
-	php8-pdo_dblib \
-	php8-pdo_mysql \
-	php8-pdo_odbc \
-	php8-pdo_pgsql \
-	php8-pdo_sqlite \
-	php8-phar \
-	php8-posix \
-	php8-session \
-	php8-simplexml \
-	php8-soap \
-	php8-sqlite3 \
-	php8-tokenizer \
-	php8-xdebug \
-	php8-xml \
-	php8-xmlreader \
-	php8-xmlwriter \
-	php8-zip
+	php82 \
+	php82-bcmath \
+	php82-bz2 \
+	php82-ctype \
+	php82-curl \
+	php82-dom \
+	php82-fileinfo \
+	php82-fpm \
+	php82-gd \
+	php82-gettext \
+	php82-gmp \
+	php82-iconv \
+	php82-intl \
+	php82-json \
+	php82-mbstring \
+	php82-pecl-mcrypt \
+	php82-mysqli \
+	php82-mysqlnd \
+	php82-odbc \
+	php82-opcache \
+	php82-openssl \
+	php82-pdo \
+	php82-pdo_dblib \
+	php82-pdo_mysql \
+	php82-pdo_odbc \
+	php82-pdo_pgsql \
+	php82-pdo_sqlite \
+	php82-phar \
+	php82-posix \
+	php82-session \
+	php82-simplexml \
+	php82-soap \
+	php82-sqlite3 \
+	php82-tokenizer \
+	php82-xdebug \
+	php82-xml \
+	php82-xmlreader \
+	php82-xmlwriter \
+	php82-zip
 
 adduser -D -s /sbin/nologin -h /dev/null -g php-fpm php-fpm
 echo "chdir = $CHDIR" >> /etc/supervisord.conf
